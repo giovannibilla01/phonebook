7e2344e530d04f6994e58d8a6659196f86d0e275
Giovanni Billa <giovanni.billa01@gmail.com>
2023-12-14 13:19:24 -0300

    contact page
    created individual contact page and stylized this

--- config/process.php
+++ config/process.php
@@ -4,13 +4,29 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $contacts = [];
+    $id = $_GET['id'] ?? null;
 
-    $query = "SELECT * FROM contacts";
+    if (!empty($id)) {
+        // RETURN A CONTACT
+        $query = "SELECT * FROM contacts WHERE id = :id";
 
-    $stmt = $connection->prepare($query);
+        $stmt = $connection->prepare($query);
 
-    $stmt->execute();
+        $stmt->bindParam(":id", $id);
 
-    $contacts = $stmt->fetchAll();
+        $stmt->execute();
+
+        $contact = $stmt->fetch();
+    } else {
+        // RETURN ALL CONTACTS
+        $contacts = [];
+
+        $query = "SELECT * FROM contacts";
+
+        $stmt = $connection->prepare($query);
+
+        $stmt->execute();
+
+        $contacts = $stmt->fetchAll();
+    }
 ?>
--- index.php
+++ index.php
@@ -24,7 +24,7 @@
                         <td><?= $contact['name'] ?></td>
                         <td><?= $contact['phone'] ?></td>
                         <td class="actions">
-                            <a href=""><i class="fas fa-eye check-icon"></i></a>
+                            <a href="<?= $BASE_URL ?>show.php?id=<?= $contact['id'] ?>"><i class="fas fa-eye check-icon"></i></a>
                             <a href=""><i class="far fa-edit edit-icon"></i></a>
                             <button type="submit"><i class="fas fa-times delete-icon"></i></button>
                         </td>
