1e080b317cbba7d4d9a954058a6664e059f71805
Giovanni Billa <giovanni.billa01@gmail.com>
2023-12-14 13:37:52 -0300

    create page
    completed the construction of the record creation page and its css

--- create.php
+++ create.php
@@ -1,7 +1,33 @@
 <?php
     include_once ("templates/header.php");
 ?>
-    <h1>Novo Usuario</h1>
+    
+<div class="container">
+
+    <?php include_once("templates/backbtn.php"); ?>
+
+    <h1 id="main-title">Novo Contato</h1>
+
+    <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="post">
+        <input type="hidden" name="type" value="create">
+        <div class="form-group">
+            <label for="name">Nome do Contato:</label>
+            <input type="text" class="form-control" name="name" id="name" placeholder="Digite o Nome do Contato" required>
+        </div>
+        <div class="form-group">
+            <label for="phone">Telefone do Contato:</label>
+            <input type="text" class="form-control" name="phone" id="phone" placeholder="Digite o Telefone do Contato" required>
+        </div>
+        <div class="form-group">
+            <label for="observation">Observações:</label>
+            <textarea type="text" class="form-control" name="observation" id="observation" placeholder="Digite Observação sobre o Contato" rows="3">
+            </textarea>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+    </form>
+</div>
     
 <?php
     include_once ("templates/footer.php");
